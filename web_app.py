4d8fbfc13650b9ed893d3b2bc1506d3f475da4a8
jamil <jamil.bsee163@iiu.edu.pk>
2022-04-11 11:01:15 -0700

    First commit Message

---
+++ web_app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA 
+from sklearn.svm import SVC 
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+#heading
+st.write('''
+ # Explore different ML models and datasets,
+which is best among them? 
+''')
+
+#data set k nam aik box ma daal k sidebar py laga do
+dataset_name=st.sidebar.selectbox(
+    'select dataset',
+    ('Iris','Breast Cancer','Wine')
+)
+
+#aor esi k neche classifier 
+classifier_name=st.sidebar.selectbox(
+    'select classifier',
+    ('KNN','SVM','Random Forest')
+)
+
+#import data
+def get_dataset(dataset_name):
+    data=None
+    if dataset_name=="Iris":
+        data=datasets.load_iris()
+    elif dataset_name =="Wine":
+        data=datasets.load_wine()
+    else:
+        data=datasets.load_breast_cancer()
+    X=data.data 
+    y=data.target 
+    return X,y 
+
+#ab is functiom ko call krwaye gy
+X,y=get_dataset(dataset_name)   
+#ab hum dataset ki shape print krwaye gy
+st.write('shape of dataset:',X.shape)
+st.write('number of classes:',len(np.unique(y)))
+
+#deffining parameter of classifier
+def add_parameter_ui(classifier_name):
+    params=dict()
+    if classifier_name== 'SVM':
+        C=st.sidebar.slider('C',0.01,10.0)
+        params['C']=C
+    elif classifier_name== 'KNN':
+        K=st.sidebar.slider('K',1,15)
+        params['K']= K 
+    else:
+        max_depth=st.sidebar.slider('max_depth',2,15)
+        params['max_depth']=max_depth
+        n_estimators=st.sidebar.slider('n_estimators',1,100)
+        params['n_estimators']=n_estimators
+    return params
+#call this function
+params=add_parameter_ui(classifier_name)
+#ab hum calssifier bnaye gy based on classifier name and params
+def get_classifier(classifier_name,params):
+    clf=None  
+    if classifier_name== 'SVM':
+        clf=SVC(C=params['C'])
+    elif classifier_name== 'KNN':
+        clf=KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf=clf=RandomForestClassifier(n_estimators=params['n_estimators'],
+        max_depth=params['max_depth'],random_state=1234)
+    return clf
+#calling function
+clf=get_classifier(classifier_name,params)
+#split data set
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
+#training classifier
+clf.fit(X_train,y_train)
+y_pred=clf.predict(X_test)
+
+#accuracy check
+acc=accuracy_score(y_test,y_pred)
+st.write(f'Classifier={classifier_name}')
+st.write(f'Accuracy=',acc)
+
+#plot dataset using pca technique that reduce feature to 2 dim
+pca=PCA(2)
+X_projected=pca.fit_transform(X)
+
+#ab hum data 0 or 1 dimension ma slice kardain gy
+x1=X_projected[:,0]
+x2=X_projected[:,1]
+
+fig=plt.figure()
+plt.scatter(x1,x2,
+      c=y,alpha=0.8,
+      cmap='viridis')
+
+plt.xlabel('Principal component 1')
+plt.ylabel('Principal component 2')
+plt.colorbar()
+
+#plt show
+st.pyplot(fig)
